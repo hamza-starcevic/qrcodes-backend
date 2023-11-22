a862ab444b6a1d689d5b179cf5ff7b78bc85c02e
ZilicAnes <zilic.anes.21@size.ba>
2023-11-22 22:48:28 +0100

    Frontend branch and login

--- app/db/db_config.py
+++ app/db/db_config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Kenansin%402002@localhost:5432/qrcodes"
+DATABASE_URL = "postgresql://postgres:adminpass@localhost:5432/qrcodes"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
